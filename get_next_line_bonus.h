25e02b5e7fccbc15b0f451fa733897ec74986a44
ahlyel-amine <ahlyel-amine@users.noreply.github.com>
2022-11-26 18:12:00 +0100

    a

---
+++ get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aahlyel <aahlyel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/26 16:24:31 by aahlyel           #+#    #+#             */
+/*   Updated: 2022/11/26 18:00:46 by aahlyel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <string.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# ifndef FD_MAX
+#  define FD_MAX 10240
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strfreejoin(char *s1, char *s2);
+int		ft_memchr_ind(char *str, char c);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_read(int fd);
+char	*ft_line(char *line);
+char	*ft_hold(char *line);
+
+#endif
---
+++ main.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aahlyel <aahlyel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 16:31:25 by aahlyel           #+#    #+#             */
+/*   Updated: 2022/11/26 18:11:08 by aahlyel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+
+int main()
+{
+	char    *p;
+	int fd = open("aa.txt", O_RDONLY);
+
+								p =																		get_next_line(fd);
+	printf("%s",p);
+									p =																get_next_line(fd);
+	printf("%s",p);
+										p =														get_next_line(fd);
+	printf("%s",p);
+											p =												get_next_line(fd);
+	printf("%s",p);
+												p =										get_next_line(fd);
+	printf("%s",p);
+													p =								get_next_line(fd);
+	printf("%s",p);
+														p =						get_next_line(fd);
+	printf("%s",p);
+															p =				get_next_line(fd);
+	printf("%s",p);												/*|||||||*/
+															p =				get_next_line(fd);
+	printf("%s",p);
+														p =						get_next_line(fd);
+	printf("%s",p);
+													p =								get_next_line(fd);
+	printf("%s",p)
+												p =										get_next_line(fd);
+	printf("%s",p);
+											p =												get_next_line(fd);
+	printf("%s",p)
+										p =														get_next_line(fd);
+	printf("%s",p);
+									p =																get_next_line(fd);
+	printf("%s",p);
+								p =																		get_next_line(fd);
+	printf("%s",p);
+
+}
